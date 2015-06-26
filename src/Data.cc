88a6e3566563a2a4580f6537de61490d4f79863b
Steffen Heyne <heyne@ie-freiburg.mpg.de>
2015-06-26 08:28:39 +0200

    minor

--- src/Data.cc
+++ src/Data.cc
@@ -124,78 +124,6 @@ bool Data::SetGraphFromSeq(GraphClass& oG, string& currSeq) {
 	return success_status;
 }
 
-//bool Data::SetGraphFromFASTAFile(istream& in, GraphClass& oG, string& currSeq) {
-//	vector<bool> vertex_status(5, false);
-//	vertex_status[0] = true; //kernel point
-//	vertex_status[1] = true; //kind
-//	vertex_status[2] = true; //viewpoint
-//	vertex_status[3] = false; //dead
-//	vertex_status[4] = false; //abstraction
-//
-//	bool success_status = false;
-//
-//	unsigned win=mpParameters->mSeqWindow;
-//	unsigned shift = (unsigned)((double)win*mpParameters->mSeqShift);
-//
-//	if (currSeq.size() == 0){
-//		in >> std::ws;
-//	}
-//
-//	char c = in.peek();
-//	string header;
-//	bool newSeq=false;
-//	//cout << "here "<< " " << currSeq.size() << " " << in.eof() << " c "  << c << endl;
-//	if (!in.eof() && c != EOF && c=='>' && currSeq.size() == 0 ){
-//		getline(in, header,'>');
-//		getline(in, header);
-//		getline(in, currSeq,'>');
-//
-//		currSeq.erase(std::remove(currSeq.begin(), currSeq.end(), '\n'),currSeq.end());
-//		currSeq.erase(std::remove(currSeq.begin(), currSeq.end(), ' '),currSeq.end());
-//		std::transform(currSeq.begin(), currSeq.end(), currSeq.begin(), ::toupper);
-//		in.unget();
-//		if (currSeq.size()==0 || header.size()==0)
-//			throw range_error("ERROR FASTA reader - empty Sequence or header found! Header:"+header);
-//		//cout << " found seq " << header << " " << currSeq.size() << " length" << " EOF? "<< in.eof() << endl;
-//		newSeq=true;
-//	} else if (c != '>' && c != EOF && c!= '\n') {
-//		throw range_error("ERROR FASTA format error!");
-//	}
-//
-//	if (currSeq.size() > 0 ) {
-//
-//		// default case for window/shift
-//		unsigned currSize = win;
-//		// case now window/shift
-//		if (win==0){
-//			currSize = currSeq.size();
-//		} else if (win>currSeq.size()) {
-//			// case seq left is smaller than win
-//			currSize=currSeq.size();
-//		}
-//
-//		if (currSize>=win){
-//
-//			string graphSeq = currSeq.substr(0,currSize);
-//			//cout << graphSeq << " " << currSeq.size() << " " << currSize << " " << in.eof() << endl;
-//			unsigned real_vertex_index = oG.InsertVertex();
-//			vector<string> vertex_symbolic_attribute_list(1);
-//			vertex_symbolic_attribute_list[0] = graphSeq;
-//			oG.SetVertexSymbolicAttributeList(real_vertex_index, vertex_symbolic_attribute_list);
-//			oG.SetVertexStatusAttributeList(real_vertex_index, vertex_status);
-//		} else if (newSeq){
-//			throw range_error("ERROR FASTA reader! Too short sequence found. Either use win=0 or increase window size!");
-//		}
-//
-//		if (win>0 && currSeq.size()-shift>=win){
-//			currSeq.erase(0,shift);
-//		} else if (currSeq.size()-shift<win || win == 0)
-//			currSeq="";
-//		success_status = true;
-//	}
-//	return success_status;
-//}
-
 bool Data::SetGraphFromFASTAFile(istream& in, GraphClass& oG, string& currSeq, unsigned& pos, string& name) {
 
 	bool success_status = false;
@@ -325,27 +253,3 @@ void Data::LoadStringList(string aFileName, vector<string>& oList, uint numToken
 	fin.close();
 	cout << ".. read: " << oList.size() << " values." << endl;
 }
-
-//void Data::LoadIndex() {
-//if (mpParameters->mRowIndexFileName != "")
-//	if (mRowIndexList.size() == 0) {
-//		LoadUnsignedList(mpParameters->mRowIndexFileName, mRowIndexList);
-//		mIndexIsLoaded = true;
-//	}
-//if (mpParameters->mColIndexFileName != "")
-//	if (mColIndexList.size() == 0) {
-//		LoadUnsignedList(mpParameters->mColIndexFileName, mColIndexList);
-//		mIndexIsLoaded = true;
-//	}
-//
-//if (IsDataLoaded() == false)
-//	throw range_error("ERROR Data::LoadIndex: Cannot assign indices before reading data file.");
-//
-//if (mRowIndexList.size() == 0) {
-//	if (!mpParameters->mMinimalOutput)
-//		cout << endl << "No row index list specified. Assuming all " << Size() << " row indices as valid." << endl;
-//	for (unsigned i = 0; i < Size(); ++i)
-//		mRowIndexList.push_back(i);
-//}
-//
-//}
